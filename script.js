61f078610ace41bd2390103a0a1c7d40d952a642
Elena Musat <emailelena_catalina_musat@yahoo.com>
2024-04-07 18:36:45 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,181 @@
+// 1. Sa se creeze o functie care primeste trei parametrii ( d, e și f ) si returneaza produsul lor
+
+// let CalculeazaParametrii = (d, e, f) => {
+//     let produs = d*e*f;
+//     return produs;
+
+// };
+
+// let produsParametrii = CalculeazaParametrii(2, 4, 6);
+
+// console.log (produsParametrii);
+
+
+ // 2. Sa se creeze o functie care primeste un parametru ( x ) si returneaza diferența lui cu 10.
+//  De exemplu dacă x = 16, funcția sa returneze 6
+
+// let x = 16;
+
+// let diferentaParametru = (x) => {
+//     let diferenta = x - 10;
+//     return diferenta;
+// };
+
+// // let diferenta = parametru(16);
+
+// console.log (diferentaParametru (x));
+
+
+
+// 3. Sa se creeze o functie care primeste un parametru ( a ) si returneaza opusul lui.
+//  De exemplu dacă a este 7 funcția sa returneze -7.
+
+// let a = 7;
+
+// let valoare = (a) => {
+    
+//     return opusValoare= -7
+
+// };
+
+
+// console.log (valoare(a));
+
+
+
+
+
+// 4. Sa se creeze o functie care primeste doi parametrii ( x si y ) si returneaza diferenta lor 
+// inmultita cu 2. Ex: dacă x = 24, y = 4, funcția sa returneze valoarea 40, adica (24 - 4) * 2
+
+
+// let x = 24;
+// let y = 4;
+// let diferenta = 0;
+// let produs = 1;
+
+// let diferentaInmultitaCuDoi = (diferenta) => {
+//     diferenta = x - y;
+//         return diferenta * 2;
+    
+//     };
+    
+    
+//     console.log (diferentaInmultitaCuDoi(diferenta));
+
+
+
+
+
+// 5. Sa se creeze o functie care primeste un parametru ( notaMea ) si returneaza “Promovat”
+//  dacă valoarea parametrului este peste 5 ( inclusiv ), în caz contrar returnează “Picat”
+
+// function verificaNota(notaMea) {
+//     if (notaMea >= 5) {
+//         return "Promovat";
+//     } else {
+//         return "Picat";
+//     }
+// }
+
+// // let notaMea = 7;
+// // console.log(verificaNota(notaMea)); 
+
+//  let notaMea = 3;
+// console.log(verificaNota(notaMea)); 
+
+
+
+
+
+// 6. Sa se creeze o funcție care primeste doi parametrii ( a si b ) și dacă diferența lor ( a - b ) 
+// este mai mare decât 10, atunci sa returneze true, altfel false.
+
+
+// let a = 20;
+// let b = 10;
+// let diferenta = 0;
+
+// function verificaDiferenta(diferenta) {
+//     diferenta = a - b;
+//         if (diferenta >= 10) {
+//             return "true";
+//         } else {
+//             return "false";
+//         }
+//     }
+
+//     console.log (verificaDiferenta(diferenta));
+
+
+
+
+// 7. Sa se creeze o funcție care primeste doi parametrii ( a si b ) și dacă cele doua numere
+//  sunt egale sa se returneze true, altfel false.
+
+
+// function suntEgale(a, b) {
+//     return a === b;
+// }
+
+
+// console.log(suntEgale(10, 5)); 
+
+
+
+// 8. Sa se creeze o funcție care primeste doi parametrii ( a si b ) și dacă este a mai mare
+//  decat b, atunci sa se returneze a, altfel b.
+
+// function aMaiMare (a, b) {
+//     if (a > b) {
+//     return a;
+// } else {
+//     return b;
+// }
+// };
+
+
+// console.log(aMaiMare(10, 5)); 
+
+
+
+
+// 9. Sa se creeze un array cu 3 elemente. Puteti sa il denumiti "primulMeuArray". Apoi:
+
+// let primulMeuArray = ['Luni', 'Marti', 'Miercuri'];
+
+// - sa se afiseze in consola elementul de pe indexul 2
+
+// console.log (primulMeuArray[2]);
+
+
+// - sa se stearga elementul de pe indexul 2
+
+// primulMeuArray.splice(2, 1);
+// console.log (primulMeuArray);
+
+// - sa se afiseze in consola tot array-ul
+
+// console.log (primulMeuArray);
+
+// - sa se adauge in array inca un element
+
+// primulMeuArray.push ('Joi');
+
+
+// - sa se afiseze in consola tot array-ul
+
+// primulMeuArray.push ('Joi');
+
+// console.log (primulMeuArray)
+
+
+// - sa se stearga elementul nou adaugat
+
+// primulMeuArray.splice(3, 1);
+
+
+
+// // - sa se afiseze in consola tot array-ul
+
+// console.log (primulMeuArray);
